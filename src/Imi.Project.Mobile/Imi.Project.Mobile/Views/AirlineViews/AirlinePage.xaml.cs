19b337edffb2a0ae169460d207db188f0906e6d0
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-02-28 16:32:35 +0100

    Add content and functionality to Airline pages #12

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Views/AirlineViews/AirlinePage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Views/AirlineViews/AirlinePage.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Imi.Project.Mobile.Core.Domain.Models;
+using Imi.Project.Mobile.Core.Domain.Services;
+using Imi.Project.Mobile.Core.Domain.Services.Mocking;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,50 @@ namespace Imi.Project.Mobile.Views.AirlineViews
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AirlinePage : ContentPage
     {
+        private readonly IAirlineService airlineService;
+
         public AirlinePage()
         {
             InitializeComponent();
+
+            airlineService = new MockAirlineService();
+        }
+
+        private async Task ListInit()
+        {
+            busyIndicator.IsVisible = true;
+
+            IEnumerable<Airline> airlines = await airlineService.GetAirlines();
+
+            lvAirlines.ItemsSource = airlines;
+            busyIndicator.IsVisible = false;
+        }
+
+        protected override async void OnAppearing()
+        {
+            await ListInit();
+            base.OnAppearing();
+        }
+
+        private async void BtnAddAirline_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AirlineFormPage(null));
+        }
+
+        private async void MnuEditAirline_Clicked(object sender, EventArgs e)
+        {
+            Airline selectedAirline = ((MenuItem)sender).CommandParameter as Airline;
+            await Navigation.PushAsync(new AirlineFormPage(selectedAirline));
+        }
+
+        private async void MnuDeleteAirline_Clicked(object sender, EventArgs e)
+        {
+            busyIndicator.IsVisible = true;
+
+            Airline selectedAirline = ((MenuItem)sender).CommandParameter as Airline;
+            await airlineService.DeleteAirline(selectedAirline.Id);
+
+            busyIndicator.IsVisible = false;
         }
     }
 }
