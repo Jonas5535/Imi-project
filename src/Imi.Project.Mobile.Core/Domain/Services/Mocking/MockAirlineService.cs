0f0db33075bcaa70929cc80056172c15403c7543
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-02-28 13:26:15 +0100

    Add MockAirlineService + implement IAirlineService #12

---
+++ src/Imi.Project.Mobile.Core/Domain/Services/Mocking/MockAirlineService.cs
@@ -0,0 +1,48 @@
+﻿using Imi.Project.Mobile.Core.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Mobile.Core.Domain.Services.Mocking
+{
+    public class MockAirlineService : IAirlineService
+    {
+        private static ObservableCollection<Airline> _airlineList;
+
+        public async Task<Airline> AddAirline(Airline airline)
+        {
+            _airlineList.Add(airline);
+            return await Task.FromResult(airline);
+        }
+
+        public async Task<Airline> DeleteAirline(Guid airlineId)
+        {
+            Airline airline = _airlineList.FirstOrDefault(a => a.Id == airlineId);
+            _airlineList.Remove(airline);
+            return await Task.FromResult(airline);
+        }
+
+        public async Task<Airline> GetAirlineById(Guid airlineId)
+        {
+            Airline airline = _airlineList.FirstOrDefault(a => a.Id == airlineId);
+            return await Task.FromResult(airline);
+        }
+
+        public async Task<IEnumerable<Airline>> GetAirlines()
+        {
+            IEnumerable<Airline> airlines = _airlineList;
+            return await Task.FromResult(airlines);
+        }
+
+        public async Task<Airline> UpdateAirline(Airline airline)
+        {
+            Airline EditedAirline = _airlineList.FirstOrDefault(a => a.Id == airline.Id);
+            _airlineList.Remove(EditedAirline);
+            _airlineList.Add(EditedAirline);
+            return await Task.FromResult(EditedAirline);
+        }
+    }
+}
