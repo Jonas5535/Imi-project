385219d10e3bb71e87b6bb17163aef9f81cea9aa
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-02-28 15:00:18 +0100

    Add all required pages #12

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Views/AirportViews/AirportFormPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Imi.Project.Mobile.Views.AirportViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AirportFormPage : ContentPage
+    {
+        public AirportFormPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
