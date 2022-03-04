2839e4008a47b307941dcd6deeaa18eb8b2d4931
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-04 10:45:31 +0100

    Add mocking data to MockAircraftService #21

--- src/Imi.Project.Blazor.Core/CRUD/Models/AircraftItem.cs
+++ src/Imi.Project.Blazor.Core/CRUD/Models/AircraftItem.cs
@@ -1,19 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Imi.Project.Blazor.Core.CRUD.Models
 {
     public class AircraftItem
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Registration { get; set; }
         public bool HasSpecialLivery { get; set; }
         public DateTime FirstSeen { get; set; }
         public DateTime LastSeen { get; set; }
-        public Guid AircraftTypeId { get; set; }
+        public string AircraftTypeId { get; set; }
         public InputSelectItem[] AircraftTypes { get; set; }
-        public Guid AirlineId { get; set; }
+        public string AirlineId { get; set; }
         public InputSelectItem[] Airlines { get; set; }
-        public Guid AirportId { get; set; }
+        public ICollection<string> AirportIds { get; set; }
         public InputSelectItem[] Airports { get; set; }
     }
 }
---
+++ src/Imi.Project.Blazor.Core/CRUD/Services/MockAircraftService.cs
@@ -0,0 +1,66 @@
+﻿using Imi.Project.Blazor.Core.CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Blazor.Core.CRUD.Services
+{
+    public class MockAircraftService : ICRUDService<AircraftListItem, AircraftItem>
+    {
+        private static InputSelectItem[] aircraftTypes = new InputSelectItem[]
+        {
+            new InputSelectItem() { Value = "1", Label = "A320"},
+            new InputSelectItem() { Value = "2", Label = "B788"},
+            new InputSelectItem() { Value = "3", Label = "MD11"}
+        };
+
+        private static InputSelectItem[] airlines = new InputSelectItem[]
+        {
+            new InputSelectItem(){ Value = "1", Label = "Brussels Airlines"},
+            new InputSelectItem(){ Value = "2", Label = "TUI Fly"},
+            new InputSelectItem(){ Value = "3", Label = "LuftHansa Cargo"}
+        };
+
+        private static InputSelectItem[] airports = new InputSelectItem[]
+        {
+            new InputSelectItem{ Value = "1", Label = "Brussels Airport"},
+            new InputSelectItem{ Value = "2", Label = "Nice Côte d'Azur"},
+            new InputSelectItem{ Value = "3", Label = "Tokyo Narita Intl. Airport"},
+        };
+
+        private static List<AircraftItem> aircrafts = new List<AircraftItem>()
+        {
+            new AircraftItem{ Id = Guid.NewGuid(), Registration = "OO-SNJ", AircraftTypeId = "1", AirlineId = "1", HasSpecialLivery = false,
+                FirstSeen = new DateTime(2021, 07, 08), LastSeen = new DateTime(2021, 07, 08), AirportIds = new List<string>{ "1", "2", "3" } },
+            new AircraftItem{ Id = Guid.NewGuid(), Registration = "OO-LOE", AircraftTypeId = "2", AirlineId = "2", HasSpecialLivery = false,
+                FirstSeen = new DateTime(2020, 07, 13), LastSeen = new DateTime(2021, 07, 08), AirportIds = new List<string>{ "1", "2" } },
+            new AircraftItem{ Id = Guid.NewGuid(), Registration = "D-ALCC", AircraftTypeId = "3", AirlineId = "3", HasSpecialLivery = false,
+                FirstSeen = new DateTime(2020, 07, 29), LastSeen = new DateTime(2021, 07, 25), AirportIds = new List<string>{ "3", } },
+        };
+
+        public Task AddAsync(AircraftItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AircraftItem> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AircraftListItem[]> ListAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(AircraftItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
