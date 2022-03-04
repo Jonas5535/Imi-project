2c952101782c3eb58b122403c3b14ff8337f5c4b
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-04 10:16:46 +0100

    Add ICRUDService and AircraftService #21

---
+++ src/Imi.Project.Blazor.Core/CRUD/Services/ICRUDService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Blazor.Core.CRUD.Services
+{
+    public interface ICRUDService<T, K>
+    {
+        Task<T[]> ListAllAsync();
+        Task<K> GetByIdAsync(Guid id);
+        Task AddAsync(K item);
+        Task UpdateAsync(K item);
+        Task DeleteAsync(Guid id);
+    }
+}
