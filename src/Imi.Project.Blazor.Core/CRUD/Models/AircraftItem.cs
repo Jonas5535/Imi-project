a3c098b0573ffd1c08900a084fff34138c08254c
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-04 09:59:04 +0100

    Add AircraftItem #21

---
+++ src/Imi.Project.Blazor.Core/CRUD/Models/AircraftItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Imi.Project.Blazor.Core.CRUD.Models
+{
+    public class AircraftItem
+    {
+        public int Id { get; set; }
+        public string Registration { get; set; }
+        public bool HasSpecialLivery { get; set; }
+        public DateTime FirstSeen { get; set; }
+        public DateTime LastSeen { get; set; }
+        public Guid AircraftTypeId { get; set; }
+        public InputSelectItem[] AircraftTypes { get; set; }
+        public Guid AirlineId { get; set; }
+        public InputSelectItem[] Airlines { get; set; }
+        public Guid AirportId { get; set; }
+        public InputCheckboxItem[] Airports { get; set; }
+    }
+}
