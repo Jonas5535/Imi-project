05aea41196860882edb1c43ba98a4578bb5bbd72
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-04 09:15:09 +0100

    Add AircraftListItem model #21

---
+++ src/Imi.Project.Blazor.Core/CRUD/Models/AircraftListItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Imi.Project.Blazor.Core.CRUD.Models
+{
+    public class AircraftListItem
+    {
+        public Guid Id { get; set; }
+        public string Registration { get; set; }
+        public string AircraftType { get; set; }
+        public string Airline { get; set; }
+    }
+}
