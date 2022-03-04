0e08774abc94b57b1595792484e56119b36e8809
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-04 11:56:32 +0100

    Add description to home page #21

--- src/Imi.Project.Blazor/Startup.cs
+++ src/Imi.Project.Blazor/Startup.cs
@@ -1,4 +1,3 @@
-using Imi.Project.Blazor.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +27,6 @@ namespace Imi.Project.Blazor
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
