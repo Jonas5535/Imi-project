27c86095489295b6b425e974ce3af50da05f162f
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-05 17:08:53 +0100

    Add generic crudservice #28

---
+++ src/Imi.Project.Mobile.Core/Domain/Services/ICRUDService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Mobile.Core.Domain.Services
+{
+    public interface ICRUDService<T>
+    {
+        Task<IEnumerable<T>> ListAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(Guid id);
+    }
+}
