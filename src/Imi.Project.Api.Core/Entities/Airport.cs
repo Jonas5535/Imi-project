d52a4ba0915a5bed2a9c45d5e6b0c10b5e95d0a9
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-06 15:34:59 +0100

    Add entities to core project #7

---
+++ src/Imi.Project.Api.Core/Entities/Airport.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class Airport : BaseEntity
+    {
+        public string Name { get; set; }
+        public string IATACode { get; set; }
+        public string ICAOCode { get; set; }
+        public int? ElevationAMSL { get; set; }
+        public int? RunwayAmount { get; set; }
+        public int? TerminalAmount { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+        //This is a collection of aircraft that have been spotted at this airport
+        public ICollection<AircraftAtAirport> AircraftsAtAirport { get; set; }
+    }
+}
