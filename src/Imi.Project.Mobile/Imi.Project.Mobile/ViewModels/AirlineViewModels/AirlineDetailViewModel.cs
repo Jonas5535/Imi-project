dc755c78c3c55c38b723e54ac0c2a724336cbdd2
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-10 15:44:37 +0100

    Bind labels on airline detail page to corresponding props #17

--- src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AirlineViewModels/AirlineDetailViewModel.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AirlineViewModels/AirlineDetailViewModel.cs
@@ -1,11 +1,23 @@
 ﻿using FreshMvvm;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Imi.Project.Mobile.Core.Domain.Models;
+using Imi.Project.Mobile.Core.Domain.Services;
 
 namespace Imi.Project.Mobile.ViewModels
 {
     public class AirlineDetailViewModel : FreshBasePageModel
     {
+        private readonly ICRUDService<Airline> _airlineService;
+        public Airline ShownAirline { get; set; }
+
+        public AirlineDetailViewModel(ICRUDService<Airline> airlineService)
+        {
+            _airlineService = airlineService;
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+            ShownAirline = initData as Airline;
+        }
     }
 }
