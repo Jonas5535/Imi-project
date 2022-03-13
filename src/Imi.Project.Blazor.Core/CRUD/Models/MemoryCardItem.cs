91bd4784e45fa539a68322e87726b65f1991d06f
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-13 18:41:43 +0100

    Add MemoryCardItem #22

---
+++ src/Imi.Project.Blazor.Core/CRUD/Models/MemoryCardItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Imi.Project.Blazor.Core.CRUD.Models
+{
+    public class MemoryCardItem
+    {
+        public const string CoverImage = "/images/Memory/CardCover.png";
+        public string ActualImage { get; set; }
+        public string CurrentImage { get; set; }
+    }
+}
