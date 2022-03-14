490c5d658a813270107664e42e3960bf20a3a326
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-14 20:31:09 +0100

    Add Init and ReverseInit methods to AirportDetailViewModel + Bind contentlabels on AirportDetailPage to props #19

--- src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AirportViewModels/AirportDetailViewModel.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AirportViewModels/AirportDetailViewModel.cs
@@ -1,11 +1,40 @@
 ﻿using FreshMvvm;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Imi.Project.Mobile.Core.Domain.Models;
+using Imi.Project.Mobile.Core.Domain.Services;
 
 namespace Imi.Project.Mobile.ViewModels
 {
     public class AirportDetailViewModel : FreshBasePageModel
     {
+        private readonly ICRUDService<Airport> _AirportService;
+
+        public AirportDetailViewModel(ICRUDService<Airport> airportService)
+        {
+            _AirportService = airportService;
+        }
+
+        private Airport shownAirport;
+
+        public Airport ShownAirport
+        {
+            get { return shownAirport; }
+            set
+            {
+                shownAirport = value;
+                RaisePropertyChanged(nameof(shownAirport));
+            }
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+            ShownAirport = initData as Airport;
+        }
+
+        public override void ReverseInit(object returnedData)
+        {
+            ShownAirport = null;
+            ShownAirport = returnedData as Airport;
+        }
     }
 }
