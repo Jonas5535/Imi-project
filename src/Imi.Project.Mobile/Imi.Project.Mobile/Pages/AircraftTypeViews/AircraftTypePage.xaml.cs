f5e6df3b9cbce711fd09810fa76c2ccb261ec0cc
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-15 15:34:11 +0100

    Add logic to to load list of aircraft type #14

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/AircraftTypeViews/AircraftTypePage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/AircraftTypeViews/AircraftTypePage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using Imi.Project.Mobile.Core.Domain.Models;
-using Imi.Project.Mobile.Core.Domain.Services;
-using Imi.Project.Mobile.Core.Domain.Services.Mocking;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Imi.Project.Mobile.Pages
@@ -12,49 +6,49 @@ namespace Imi.Project.Mobile.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AircraftTypePage : ContentPage
     {
-        private readonly ICRUDService<AircraftType> aircraftTypeService;
+        //private readonly ICRUDService<AircraftType> aircraftTypeService;
 
         public AircraftTypePage()
         {
             InitializeComponent();
-            aircraftTypeService = new MockAircraftTypeService();
+            //aircraftTypeService = new MockAircraftTypeService();
         }
 
-        private async Task ListInit()
-        {
-            //busyIndicator.IsVisible = true;
+        //private async Task ListInit()
+        //{
+        //    //busyIndicator.IsVisible = true;
 
-            IEnumerable<AircraftType> aircraftTypes = await aircraftTypeService.ListAllAsync();
+        //    IEnumerable<AircraftType> aircraftTypes = await aircraftTypeService.ListAllAsync();
 
-            lvAircraftTypes.ItemsSource = aircraftTypes;
-            //busyIndicator.IsVisible = false;
-        }
+        //    lvAircraftTypes.ItemsSource = aircraftTypes;
+        //    //busyIndicator.IsVisible = false;
+        //}
 
-        protected override async void OnAppearing()
-        {
-            await ListInit();
-            base.OnAppearing();
-        }
+        //protected override async void OnAppearing()
+        //{
+        //    await ListInit();
+        //    base.OnAppearing();
+        //}
 
-        private async void BtnAddType_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new AircraftTypeFormPage(null));
-        }
+        //private async void BtnAddType_Clicked(object sender, EventArgs e)
+        //{
+        //    await Navigation.PushAsync(new AircraftTypeFormPage(null));
+        //}
 
-        private async void MnuEditType_Clicked(object sender, EventArgs e)
-        {
-            AircraftType selectedType = ((MenuItem)sender).CommandParameter as AircraftType;
-            await Navigation.PushAsync(new AircraftTypeFormPage(selectedType));
-        }
+        //private async void MnuEditType_Clicked(object sender, EventArgs e)
+        //{
+        //    AircraftType selectedType = ((MenuItem)sender).CommandParameter as AircraftType;
+        //    await Navigation.PushAsync(new AircraftTypeFormPage(selectedType));
+        //}
 
-        private async void MnuDeleteType_Clicked(object sender, EventArgs e)
-        {
-            //busyIndicator.IsVisible = true;
+        //private async void MnuDeleteType_Clicked(object sender, EventArgs e)
+        //{
+        //    //busyIndicator.IsVisible = true;
 
-            AircraftType selectedType = ((MenuItem)sender).CommandParameter as AircraftType;
-            await aircraftTypeService.DeleteAsync(selectedType.Id);
+        //    AircraftType selectedType = ((MenuItem)sender).CommandParameter as AircraftType;
+        //    await aircraftTypeService.DeleteAsync(selectedType.Id);
 
-            //busyIndicator.IsVisible = false;
-        }
+        //    //busyIndicator.IsVisible = false;
+        //}
     }
 }
--- src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftTypeViewModels/AircraftTypeViewModel.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftTypeViewModels/AircraftTypeViewModel.cs
@@ -1,11 +1,44 @@
 ﻿using FreshMvvm;
+using Imi.Project.Mobile.Core.Domain.Models;
+using Imi.Project.Mobile.Core.Domain.Services;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Mobile.ViewModels
 {
     public class AircraftTypeViewModel : FreshBasePageModel
     {
+        private readonly ICRUDService<AircraftType> _aircraftTypeService;
+
+        public AircraftTypeViewModel(ICRUDService<AircraftType> aircraftTypeService)
+        {
+            _aircraftTypeService = aircraftTypeService;
+        }
+
+        private ObservableCollection<AircraftType> aircraftTypes;
+
+        public ObservableCollection<AircraftType> AircraftTypes
+        {
+            get { return aircraftTypes; }
+            set
+            {
+                aircraftTypes = value;
+                RaisePropertyChanged(nameof(AircraftTypes));
+            }
+        }
+
+        protected async override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
+            await ListInit();
+        }
+
+        private async Task ListInit()
+        {
+            ObservableCollection<AircraftType> aircraftTypes = await _aircraftTypeService.ListAllAsync();
+            AircraftTypes = null;
+            AircraftTypes = aircraftTypes;
+        }
     }
 }
