7d3c3c63dfc2046660e04793a70d4c570ed27a25
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-16 10:25:00 +0100

    Add request and response dtos for aircrafttype #11

---
+++ src/Imi.Project.Api.Core/Dtos/AircraftType/AircraftTypeListResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Imi.Project.Api.Core.Dtos.AircraftType
+{
+    public class AircraftTypeListResponseDto : BaseDto
+    {
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string ICAOCode { get; set; }
+    }
+}
---
+++ src/Imi.Project.Api.Core/Dtos/AircraftType/AircraftTypeRequestDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Api.Core.Dtos.AircraftType
+{
+    public class AircraftTypeRequestDto : BaseDto
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        public string Brand { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public string Type { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public string ICAOCode { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public DateTime? FirstFlight { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public double? Length { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public double? WingSpan { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public double? Height { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public int? EmptyWeight { get; set; }
+    }
+}
