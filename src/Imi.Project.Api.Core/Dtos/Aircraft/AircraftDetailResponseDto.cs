533ad466c550ba1df3d31bbbd29282460989b32a
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-16 10:40:07 +0100

    Add request and response dots for aircraft #11

---
+++ src/Imi.Project.Api.Core/Dtos/Aircraft/AircraftDetailResponseDto.cs
@@ -0,0 +1,22 @@
+﻿using Imi.Project.Api.Core.Dtos.AircraftType;
+using Imi.Project.Api.Core.Dtos.Airline;
+using Imi.Project.Api.Core.Dtos.Airport;
+using System;
+using System.Collections.Generic;
+
+namespace Imi.Project.Api.Core.Dtos.Aircraft
+{
+    public class AircraftDetailResponseDto
+    {
+        public string Registration { get; set; }
+        public bool HasSpecialLivery { get; set; }
+        public DateTime FirstSeen { get; set; }
+        public DateTime LastSeen { get; set; }
+        public string Image { get; set; }
+        public double? Longitude { get; set; }
+        public double? Latitude { get; set; }
+        public AircraftTypeListResponseDto AircraftType { get; set; }
+        public AirlineListResponseDto Airline { get; set; }
+        public ICollection<AirportListResponseDto> Airports { get; set; }
+    }
+}
---
+++ src/Imi.Project.Api.Core/Dtos/Aircraft/AircraftRequestDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Api.Core.Dtos.Aircraft
+{
+    public class AircraftRequestDto : BaseDto
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        public string Registration { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public bool HasSpecialLivery { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public DateTime FirstSeen { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public DateTime LastSeen { get; set; }
+
+        // Image is not required as this is just an option
+        public string Image { get; set; }
+
+        // These two properties are also not required
+        // They serve as gps coördinates but not every platform has gps service
+        public double? Longitude { get; set; }
+        public double? Latitude { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public Guid AirlineId { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public Guid AircraftTypeId { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public List<Guid> AirportIds { get; set; }
+    }
+}
