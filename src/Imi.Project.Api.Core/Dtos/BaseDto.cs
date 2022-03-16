4fe0d07e7e329d587ba2f8ab95e2cc50c378171d
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-16 09:55:53 +0100

    Add methods and properties for errorhandling to BaseDto #11

--- src/Imi.Project.Api.Core/Dtos/BaseDto.cs
+++ src/Imi.Project.Api.Core/Dtos/BaseDto.cs
@@ -1,9 +1,6 @@
 ﻿using Imi.Project.Api.Core.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Dtos
 {
@@ -11,5 +8,50 @@ namespace Imi.Project.Api.Core.Dtos
     {
         public Guid Id { get; set; }
         private List<ErrorMessage> ErrorMessages { get; set; }
+        private bool _isSucces => ErrorMessages is null || ErrorMessages?.Count <= 0;
+
+        public IEnumerable<ErrorMessage> GetErrors()
+        {
+            return ErrorMessages;
+        }
+
+        public void AddError(ErrorMessage error)
+        {
+            if (ErrorMessages is null)
+            {
+                ErrorMessages = new List<ErrorMessage>();
+            }
+            ErrorMessages.Add(error);
+        }
+
+        public void AddBadRequest(string message)
+        {
+            AddError(ErrorMessage.BadRequestError(message));
+        }
+
+        public void AddNotFound(string message)
+        {
+            AddError(ErrorMessage.NotFoundError(message));
+        }
+
+        public void AddConflict(string message)
+        {
+            AddError(ErrorMessage.ConflictError(message));
+        }
+
+        public void AddInternalServerError(string message)
+        {
+            AddError(ErrorMessage.ConflictError(message));
+        }
+
+        public void AddForbidden(string message)
+        {
+            AddError(ErrorMessage.Forbidden(message));
+        }
+
+        public bool IsSucces()
+        {
+            return _isSucces;
+        }
     }
 }
