58f5c5b8e435020aa327505605fbc12468e5cfa3
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-16 11:22:20 +0100

    Add extension methods for dto and controller to help with validation #11

---
+++ src/Imi.Project.Api/Extensions/DtoValidations.cs
@@ -0,0 +1,19 @@
+﻿using Imi.Project.Api.Core.Dtos;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Imi.Project.Api.Extensions
+{
+    public static class DtoValidations
+    {
+        /// <summary>
+        /// Checks IsSucces property of every dto in a list of dtos
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static bool IsSucces(this IEnumerable<BaseDto> result)
+        {
+            return !(result?.Count() > 0 && !result.First().IsSucces());
+        }
+    }
+}
