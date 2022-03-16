65eaae4ea1f6eba5df3892a349236f7637eb858c
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-16 10:12:07 +0100

    Add Airport response and request dtos #11

---
+++ src/Imi.Project.Api.Core/Dtos/Airport/AirportListResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Imi.Project.Api.Core.Dtos.Airport
+{
+    public class AirportListResponseDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string IATACode { get; set; }
+        public string ICAOCode { get; set; }
+    }
+}
