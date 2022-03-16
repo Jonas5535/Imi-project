f5ed3fc4d8f8668a6dff188e8d21560d52220eaf
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-16 08:57:09 +0100

    Add BaseDto #11

---
+++ src/Imi.Project.Api.Core/Dtos/BaseDto.cs
@@ -0,0 +1,15 @@
+﻿using Imi.Project.Api.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class BaseDto
+    {
+        public Guid Id { get; set; }
+        private List<ErrorMessage> ErrorMessages { get; set; }
+    }
+}
