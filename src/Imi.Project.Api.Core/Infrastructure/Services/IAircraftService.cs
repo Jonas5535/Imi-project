9d3539153b8967d9ad1ae6b1492e7f0d65f7f321
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-17 15:58:39 +0100

    Add service interfaces #9

---
+++ src/Imi.Project.Api.Core/Infrastructure/Services/IAircraftService.cs
@@ -0,0 +1,8 @@
+﻿using Imi.Project.Api.Core.Dtos.Aircraft;
+
+namespace Imi.Project.Api.Core.Infrastructure.Services
+{
+    public interface IAircraftService : IBaseService<AircraftListResponseDto, AircraftDetailResponseDto, AircraftRequestDto>
+    {
+    }
+}
---
+++ src/Imi.Project.Api.Core/Infrastructure/Services/IBaseService.cs
@@ -0,0 +1,16 @@
+﻿using Imi.Project.Api.Core.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Infrastructure.Services
+{
+    public interface IBaseService<T, D, R> where T : BaseDto where D : BaseDto where R : BaseDto
+    {
+        Task<IEnumerable<T>> ListAllAsync();
+        Task<D> GetByIdAsync(Guid id);
+        Task<D> AddAsync(R requestDto);
+        Task<D> UpdateAsync(R requestDto);
+        Task DeleteAsync(Guid id);
+    }
+}
