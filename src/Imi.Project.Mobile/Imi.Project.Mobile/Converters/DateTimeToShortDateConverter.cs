e7330c016fcee085ea1bf805a59314215798e535
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-18 15:22:06 +0100

    Add DateTimeToShortDate converter #18

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Converters/DateTimeToShortDateConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.Converters
+{
+    public class DateTimeToShortDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime date)
+            {
+                string shortDate = date.ToShortDateString();
+                return shortDate;
+            }
+            throw new ArgumentException($"{nameof(value)} must be of value DateTime)");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
