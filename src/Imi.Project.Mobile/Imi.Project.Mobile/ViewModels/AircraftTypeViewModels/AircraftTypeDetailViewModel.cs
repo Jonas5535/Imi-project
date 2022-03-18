dbf05a28278223995df35e0caaee24912a8bd315
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-18 14:58:13 +0100

    Add logic to get AircraftTypeDetails and display them #18

--- src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftTypeViewModels/AircraftTypeDetailViewModel.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftTypeViewModels/AircraftTypeDetailViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using FreshMvvm;
+using Imi.Project.Mobile.Core.Domain.Models;
+using Imi.Project.Mobile.Core.Domain.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +9,35 @@ namespace Imi.Project.Mobile.ViewModels
 {
     public class AircraftTypeDetailViewModel : FreshBasePageModel
     {
+        private readonly ICRUDService<AircraftType> _aircraftTypeService;
 
+        public AircraftTypeDetailViewModel(ICRUDService<AircraftType> aircraftTypeService)
+        {
+            _aircraftTypeService = aircraftTypeService;
+        }
+
+        private AircraftType shownType;
+
+        public AircraftType ShownType
+        {
+            get { return shownType; }
+            set
+            {
+                shownType = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+            ShownType = initData as AircraftType;
+        }
+
+        public override void ReverseInit(object returnedData)
+        {
+            ShownType = null;
+            ShownType = returnedData as AircraftType;
+        }
     }
 }
