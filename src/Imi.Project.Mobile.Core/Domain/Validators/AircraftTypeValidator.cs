d5da9df5673354a9e2767b90dad5db757d1dfe28
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-20 13:34:14 +0100

    Add validation rules for detail props of aircraft type #18

--- src/Imi.Project.Mobile.Core/Domain/Validators/AircraftTypeValidator.cs
+++ src/Imi.Project.Mobile.Core/Domain/Validators/AircraftTypeValidator.cs
@@ -24,6 +24,30 @@ namespace Imi.Project.Mobile.Core.Domain.Validators
                 .WithMessage("De ICAO code mag niet leeg zijn")
                 .Length(4)
                 .WithMessage("De ICAO code van een vliegtuigtype moet 4 karakters lang zijn");
+
+            RuleFor(aircraftType => aircraftType.Length)
+                .NotEmpty()
+                .WithMessage("De lengte mag niet leeg zijn")
+                .GreaterThan(0)
+                .WithMessage("De lengte moet groter zijn dan 0");
+
+            RuleFor(aircraftType => aircraftType.WingSpan)
+                .NotEmpty()
+                .WithMessage("De spanwijdte mag niet leeg zijn")
+                .GreaterThan(0)
+                .WithMessage("De spanwijdte moet groter zijn dan 0");
+
+            RuleFor(aicraftType => aicraftType.Height)
+                .NotEmpty()
+                .WithMessage("De hoogte mag niet leeg zijn")
+                .GreaterThan(0)
+                .WithMessage("De hoogte moet groter zijn dan 0");
+
+            RuleFor(aircraftType => aircraftType.EmptyWeight)
+                .NotEmpty()
+                .WithMessage("Het leeggewicht mag niet leeg zijn")
+                .GreaterThan(0)
+                .WithMessage("Het leeggewicht moet groter zijn dan 0");
         }
     }
 }
