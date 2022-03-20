b9312e0efb419ad75216b3d177652059ef22c88e
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-20 14:55:53 +0100

    Add converter to add unit next to numeric value and use where necessary #18

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Converters/NumberToNumberWithUnitConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.Converters
+{
+    public class NumberToNumberWithUnitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string valueWithUnit = $"{value} {parameter}";
+            return valueWithUnit;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
