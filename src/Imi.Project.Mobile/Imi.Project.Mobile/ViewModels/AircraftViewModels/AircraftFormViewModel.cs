9def744d01d695a9e10de44ac9167ca764ef458b
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-21 13:58:49 +0100

    Add properties to AircraftFormViewModel #16

--- src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftViewModels/AircraftFormViewModel.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftViewModels/AircraftFormViewModel.cs
@@ -1,8 +1,224 @@
-﻿using FreshMvvm;
+﻿using FluentValidation;
+using FreshMvvm;
+using Imi.Project.Mobile.Core.Domain.Models;
+using Imi.Project.Mobile.Core.Domain.Services;
+using System;
+using System.Collections.Generic;
 
 namespace Imi.Project.Mobile.ViewModels
 {
     public class AircraftFormViewModel : FreshBasePageModel
     {
+        private readonly ICRUDService<Aircraft> _aircraftService;
+        private readonly ICRUDService<AircraftType> _aircraftTypeService;
+        private readonly ICRUDService<Airline> _airlineService;
+        private readonly ICRUDService<Airport> _airportService;
+        private readonly IValidator<Aircraft> _aircraftValidator;
+        private Aircraft _currentAircraft;
+        private bool _isNew = true;
+
+        public AircraftFormViewModel(ICRUDService<Aircraft> aircraftService, ICRUDService<AircraftType> aircraftTypeService,
+            ICRUDService<Airline> airlineService, ICRUDService<Airport> airportService)
+        {
+            _aircraftService = aircraftService;
+            _aircraftTypeService = aircraftTypeService;
+            _airlineService = airlineService;
+            _airportService = airportService;
+        }
+
+        #region Properties
+        private string pageTitle;
+
+        public string PageTitle
+        {
+            get { return pageTitle; }
+            set
+            {
+                pageTitle = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private bool isBusy;
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set
+            {
+                isBusy = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string registration;
+
+        public string Registration
+        {
+            get { return registration; }
+            set
+            {
+                registration = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string registrationError;
+
+        public string RegistrationError
+        {
+            get { return registrationError; }
+            set
+            {
+                registrationError = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        private bool hasSpecialLivery;
+
+        public bool HasSpecialLivery
+        {
+            get { return hasSpecialLivery; }
+            set
+            {
+                hasSpecialLivery = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string hasSpecialLiveryError;
+
+        public string HasSpecialLiveryError
+        {
+            get { return hasSpecialLiveryError; }
+            set
+            {
+                hasSpecialLiveryError = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private DateTime firstSeen;
+
+        public DateTime FirstSeen
+        {
+            get { return firstSeen; }
+            set
+            {
+                firstSeen = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string firstSeenError;
+
+        public string FirstSeenError
+        {
+            get { return firstSeenError; }
+            set
+            {
+                firstSeenError = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private DateTime lastSeen;
+
+        public DateTime LastSeen
+        {
+            get { return lastSeen; }
+            set
+            {
+                lastSeen = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string lastSeenError;
+
+        public string LastSeenError
+        {
+            get { return lastSeenError; }
+            set
+            {
+                lastSeenError = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private AircraftType aircraftType;
+
+        public AircraftType AircraftType
+        {
+            get { return aircraftType; }
+            set
+            {
+                aircraftType = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string aircraftTypeError;
+
+        public string AircraftTypeError
+        {
+            get { return aircraftTypeError; }
+            set
+            {
+                aircraftTypeError = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private Airline airline;
+
+        public Airline Airline
+        {
+            get { return airline; }
+            set
+            {
+                airline = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string airlineError;
+
+        public string AirlineError
+        {
+            get { return airlineError; }
+            set
+            {
+                airlineError = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private ICollection<Airport> airports;
+
+        public ICollection<Airport> Airports
+        {
+            get { return airports; }
+            set
+            {
+                airports = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string airportError;
+
+        public string AirportError
+        {
+            get { return airportError; }
+            set
+            {
+                airportError = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
     }
 }
