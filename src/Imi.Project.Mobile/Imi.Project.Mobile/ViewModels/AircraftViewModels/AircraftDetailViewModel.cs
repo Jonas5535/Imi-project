961904d91726f835123f7ae69e14a66c2dc218e3
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-21 10:29:09 +0100

    Add logic to load aircraft details to aircraft viewmodel #16

--- src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftViewModels/AircraftDetailViewModel.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftViewModels/AircraftDetailViewModel.cs
@@ -1,8 +1,40 @@
 ﻿using FreshMvvm;
+using Imi.Project.Mobile.Core.Domain.Models;
+using Imi.Project.Mobile.Core.Domain.Services;
 
 namespace Imi.Project.Mobile.ViewModels
 {
     public class AircraftDetailViewModel : FreshBasePageModel
     {
+        private readonly ICRUDService<Aircraft> _aircraftService;
+
+        public AircraftDetailViewModel(ICRUDService<Aircraft> aircraftService)
+        {
+            _aircraftService = aircraftService;
+        }
+
+        private Aircraft shownAircraft;
+
+        public Aircraft ShownAircraft
+        {
+            get { return shownAircraft; }
+            set
+            {
+                shownAircraft = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+            ShownAircraft = initData as Aircraft;
+        }
+
+        public override void ReverseInit(object returnedData)
+        {
+            ShownAircraft = null;
+            ShownAircraft = returnedData as Aircraft;
+        }
     }
 }
