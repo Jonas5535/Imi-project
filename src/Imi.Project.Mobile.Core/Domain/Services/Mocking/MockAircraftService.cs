6161f0a2efe0ca74ac7044833ea777fa57f09a63
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-21 11:02:57 +0100

    Rework mocking data for aircraft #16

--- src/Imi.Project.Mobile.Core/Domain/Services/Mocking/MockAircraftService.cs
+++ src/Imi.Project.Mobile.Core/Domain/Services/Mocking/MockAircraftService.cs
@@ -9,19 +9,40 @@ namespace Imi.Project.Mobile.Core.Domain.Services.Mocking
 {
     public class MockAircraftService : ICRUDService<Aircraft>
     {
+        private static Airline[] airlines = new Airline[]
+        {
+            new Airline{ Id = Guid.NewGuid(), Name = "Brussels Airlines", IATACode = "SN", ICAOCode = "BEL"},
+            new Airline{ Id = Guid.NewGuid(), Name = "TUI Fly", IATACode = "TB", ICAOCode = "JAF"},
+            new Airline{ Id = Guid.NewGuid(), Name = "Lufthansa Cargo", IATACode = "LH", ICAOCode = "GEC"}
+        };
+
+        private static AircraftType[] aircraftTypes = new AircraftType[]
+        {
+            new AircraftType{ Id = Guid.NewGuid(), Brand = "Airbus", Type = "A320-200", ICAOCode = "A320"},
+            new AircraftType{ Id = Guid.NewGuid(), Brand = "Boeing", Type = "787-8", ICAOCode = "B788"},
+            new AircraftType{ Id = Guid.NewGuid(), Brand = "McDonnel Douglas", Type = "MD-11", ICAOCode = "MD11"},
+        };
+
+        private static Airport[] airports = new Airport[]
+        {
+            new Airport{ Id = Guid.NewGuid(), Name = "Brussels Airport", IATACode = "BRU", ICAOCode = "EBBR"},
+            new Airport{ Id = Guid.NewGuid(), Name = "Nice Côte d'Azur", IATACode = "NCE", ICAOCode = "LFMN"},
+            new Airport{ Id = Guid.NewGuid(), Name = "Tokyo Narita Intl. Airport", IATACode = "NRT", ICAOCode = "RJAA"},
+        };
+
         private static ObservableCollection<Aircraft> _aircraftList = new ObservableCollection<Aircraft>
         {
-            new Aircraft{ Id = Guid.NewGuid(),Registration = "OO-SNJ", AircraftType = "A320", Airline = "Brussels Airlines", HasSpecialLivery = false,
+            new Aircraft{ Id = Guid.NewGuid(),Registration = "OO-SNJ", AircraftType = aircraftTypes[0], Airline = airlines[0], HasSpecialLivery = false,
                 FirstSeen = new DateTime(2021, 07, 08), LastSeen = new DateTime(2021, 07, 08),
-                Airports = new ObservableCollection<string>{ "Brussels Airport", "Nice Côte d'Azur", "Tokyo Narita Intl. Airport"}
+                Airports = new ObservableCollection<Airport>{ airports[0], airports[1], airports[2]}
             },
-            new Aircraft{ Id = Guid.NewGuid(), Registration = "OO-LOE", AircraftType = "B788", Airline = "TUI Fly", HasSpecialLivery = false,
+            new Aircraft{ Id = Guid.NewGuid(), Registration = "OO-LOE", AircraftType = aircraftTypes[1], Airline = airlines[1], HasSpecialLivery = false,
                 FirstSeen = new DateTime(2020, 07, 13), LastSeen = new DateTime(2021, 07, 08),
-                Airports = new List<string>{ "Brussels Airport", "Nice Côte d'Azur" }
+                Airports = new List<Airport>{ airports[0], airports[1] }
             },
-            new Aircraft{ Id = Guid.NewGuid(), Registration = "D-ALCC", AircraftType = "MD11", Airline = "Lufthansa Cargo", HasSpecialLivery = false,
+            new Aircraft{ Id = Guid.NewGuid(), Registration = "D-ALCC", AircraftType = aircraftTypes[2], Airline = airlines[2], HasSpecialLivery = false,
                 FirstSeen = new DateTime(2020, 07, 29), LastSeen = new DateTime(2021, 07, 25),
-                Airports = new List<string>{ "Tokyo Narita Intl. Airport" }
+                Airports = new List<Airport>{ airports[2] }
             }
         };
 
--- src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/AircraftViews/AircraftFormPage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/AircraftViews/AircraftFormPage.xaml.cs
@@ -15,258 +15,258 @@ namespace Imi.Project.Mobile.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AircraftFormPage : ContentPage
     {
-        private readonly ICRUDService<Aircraft> aircraftService;
-        private readonly ICRUDService<AircraftType> aircraftTypeService;
-        private readonly ICRUDService<Airline> airlineService;
-        private readonly ICRUDService<Airport> airportService;
-        private Aircraft currentAircraft;
-        private bool isNew = true;
-        private List<Picker> pickers = new List<Picker>();
-        private IValidator aircraftValidator;
+        //private readonly ICRUDService<Aircraft> aircraftService;
+        //private readonly ICRUDService<AircraftType> aircraftTypeService;
+        //private readonly ICRUDService<Airline> airlineService;
+        //private readonly ICRUDService<Airport> airportService;
+        //private Aircraft currentAircraft;
+        //private bool isNew = true;
+        //private List<Picker> pickers = new List<Picker>();
+        //private IValidator aircraftValidator;
 
         public AircraftFormPage(Aircraft aircraft)
         {
             InitializeComponent();
 
-            aircraftService = new MockAircraftService();
-            aircraftTypeService = new MockAircraftTypeService();
-            airlineService = new MockAirlineService();
-            airportService = new MockAirportService();
-            aircraftValidator = new AircraftValidator();
+            //aircraftService = new MockAircraftService();
+            //aircraftTypeService = new MockAircraftTypeService();
+            //airlineService = new MockAirlineService();
+            //airportService = new MockAirportService();
+            //aircraftValidator = new AircraftValidator();
 
-            if (aircraft == null)
-            {
-                currentAircraft = new Aircraft();
-                Title = "Nieuw vliegtuig";
-            }
-            else
-            {
-                isNew = false;
-                currentAircraft = aircraft;
-                Title = $"{currentAircraft.Registration} bewerken";
-            }
+            //if (aircraft == null)
+            //{
+            //    currentAircraft = new Aircraft();
+            //    Title = "Nieuw vliegtuig";
+            //}
+            //else
+            //{
+            //    isNew = false;
+            //    currentAircraft = aircraft;
+            //    Title = $"{currentAircraft.Registration} bewerken";
+            //}
         }
 
-        protected override void OnAppearing()
-        {
-            PopulateTypePicker();
-            PopulateAirlinePicker();
-            PopulateAirportPicker(pckAirport);
-            pickers.Add(pckAirport);
+        //protected override void OnAppearing()
+        //{
+        //    PopulateTypePicker();
+        //    PopulateAirlinePicker();
+        //    PopulateAirportPicker(pckAirport);
+        //    pickers.Add(pckAirport);
 
-            if (!isNew)
-            {
-                LoadAircraftState();
-            }
-            base.OnAppearing();
-        }
+        //    if (!isNew)
+        //    {
+        //        LoadAircraftState();
+        //    }
+        //    base.OnAppearing();
+        //}
 
-        private async void PopulateTypePicker()
-        {
-            IEnumerable<AircraftType> types = await aircraftTypeService.ListAllAsync();
-            List<string> pickerContent = new List<string>();
+        //private async void PopulateTypePicker()
+        //{
+        //    IEnumerable<AircraftType> types = await aircraftTypeService.ListAllAsync();
+        //    List<string> pickerContent = new List<string>();
 
-            foreach (var type in types)
-            {
-                pickerContent.Add(type.ICAOCode);
-            }
+        //    foreach (var type in types)
+        //    {
+        //        pickerContent.Add(type.ICAOCode);
+        //    }
 
-            pckType.ItemsSource = pickerContent;
-        }
+        //    pckType.ItemsSource = pickerContent;
+        //}
 
-        private async void PopulateAirlinePicker()
-        {
-            IEnumerable<Airline> airlines = await airlineService.ListAllAsync();
-            List<string> pickerContent = new List<string>();
+        //private async void PopulateAirlinePicker()
+        //{
+        //    IEnumerable<Airline> airlines = await airlineService.ListAllAsync();
+        //    List<string> pickerContent = new List<string>();
 
-            foreach (var airline in airlines)
-            {
-                pickerContent.Add(airline.Name);
-            }
+        //    foreach (var airline in airlines)
+        //    {
+        //        pickerContent.Add(airline.Name);
+        //    }
 
-            pckAirline.ItemsSource = pickerContent;
-        }
+        //    pckAirline.ItemsSource = pickerContent;
+        //}
 
-        private async void PopulateAirportPicker(Picker picker)
-        {
-            IEnumerable<Airport> airports = await airportService.ListAllAsync();
-            List<string> pickerContent = new List<string>();
+        //private async void PopulateAirportPicker(Picker picker)
+        //{
+        //    IEnumerable<Airport> airports = await airportService.ListAllAsync();
+        //    List<string> pickerContent = new List<string>();
 
-            foreach (var airport in airports)
-            {
-                pickerContent.Add(airport.Name);
-            }
+        //    foreach (var airport in airports)
+        //    {
+        //        pickerContent.Add(airport.Name);
+        //    }
 
-            picker.ItemsSource = pickerContent;
-        }
+        //    picker.ItemsSource = pickerContent;
+        //}
 
-        private void LoadAircraftState()
-        {
-            txtRegistration.Text = currentAircraft.Registration;
-            pckType.SelectedItem = currentAircraft.AircraftType;
-            pckAirline.SelectedItem = currentAircraft.Airline;
-            swLivery.IsToggled = currentAircraft.HasSpecialLivery;
-            dpFirstSeen.Date = currentAircraft.FirstSeen.Date;
-            dpLastSeen.Date = currentAircraft.LastSeen.Date;
+        //private void LoadAircraftState()
+        //{
+        //    txtRegistration.Text = currentAircraft.Registration;
+        //    pckType.SelectedItem = currentAircraft.AircraftType;
+        //    pckAirline.SelectedItem = currentAircraft.Airline;
+        //    swLivery.IsToggled = currentAircraft.HasSpecialLivery;
+        //    dpFirstSeen.Date = currentAircraft.FirstSeen.Date;
+        //    dpLastSeen.Date = currentAircraft.LastSeen.Date;
 
-            if (!isNew)
-            {
-                pckAirport.SelectedIndexChanged -= pckAirport_SelectedIndexChanged;
-                pckAirport.SelectedItem = currentAircraft.Airports.FirstOrDefault();
-                for (int i = 1; i < currentAircraft.Airports.Count; i++)
-                {
-                    Picker newPicker = AddPicker();
-                    newPicker.SelectedIndexChanged -= pckAirport_SelectedIndexChanged;
-                    newPicker.SelectedItem = currentAircraft.Airports.ElementAt(i);
-                }
-                AddPicker();
-            }
-        }
+        //    if (!isNew)
+        //    {
+        //        pckAirport.SelectedIndexChanged -= pckAirport_SelectedIndexChanged;
+        //        pckAirport.SelectedItem = currentAircraft.Airports.FirstOrDefault();
+        //        for (int i = 1; i < currentAircraft.Airports.Count; i++)
+        //        {
+        //            Picker newPicker = AddPicker();
+        //            newPicker.SelectedIndexChanged -= pckAirport_SelectedIndexChanged;
+        //            newPicker.SelectedItem = currentAircraft.Airports.ElementAt(i);
+        //        }
+        //        AddPicker();
+        //    }
+        //}
 
-        private void SaveAircraftState()
-        {
-            currentAircraft.Registration = txtRegistration.Text;
+        //private void SaveAircraftState()
+        //{
+        //    currentAircraft.Registration = txtRegistration.Text;
 
-            //Extra validatie om crashes tegen te gaan
-            if (pckType.SelectedItem != null)
-                currentAircraft.AircraftType = pckType.SelectedItem.ToString();
+        //    //Extra validatie om crashes tegen te gaan
+        //    if (pckType.SelectedItem != null)
+        //        currentAircraft.AircraftType = pckType.SelectedItem.ToString();
 
-            if (pckAirline.SelectedItem != null)
-                currentAircraft.Airline = pckAirline.SelectedItem.ToString();
+        //    if (pckAirline.SelectedItem != null)
+        //        currentAircraft.Airline = pckAirline.SelectedItem.ToString();
 
-            currentAircraft.HasSpecialLivery = swLivery.IsToggled;
-            currentAircraft.FirstSeen = dpFirstSeen.Date;
-            currentAircraft.LastSeen = dpLastSeen.Date;
-            if (pckAirport.SelectedItem != null)
-            {
-                currentAircraft.Airports.Clear();
-                foreach (var picker in pickers)
-                {
-                    Picker lastPicker = pickers.LastOrDefault();
+        //    currentAircraft.HasSpecialLivery = swLivery.IsToggled;
+        //    currentAircraft.FirstSeen = dpFirstSeen.Date;
+        //    currentAircraft.LastSeen = dpLastSeen.Date;
+        //    if (pckAirport.SelectedItem != null)
+        //    {
+        //        currentAircraft.Airports.Clear();
+        //        foreach (var picker in pickers)
+        //        {
+        //            Picker lastPicker = pickers.LastOrDefault();
 
-                    if (picker == lastPicker && lastPicker.SelectedItem == null)
-                    {
-                        break;
-                    }
-                    currentAircraft.Airports.Add(picker.SelectedItem.ToString());
-                }
-            }
-        }
+        //            if (picker == lastPicker && lastPicker.SelectedItem == null)
+        //            {
+        //                break;
+        //            }
+        //            currentAircraft.Airports.Add(picker.SelectedItem.ToString());
+        //        }
+        //    }
+        //}
 
-        private Picker AddPicker()
-        {
-            StackLayout stackLayout = new StackLayout { Orientation = StackOrientation.Horizontal };
+        //private Picker AddPicker()
+        //{
+        //    StackLayout stackLayout = new StackLayout { Orientation = StackOrientation.Horizontal };
 
-            AircraftForm.Children.Add(stackLayout);
+        //    AircraftForm.Children.Add(stackLayout);
 
-            Picker picker = new Picker { Title = "Kies een luchthaven", Style = (Style)Application.Current.Resources["StandardPicker"], WidthRequest = 297, Margin = new Thickness(15, 0, 0, 0) };
-            picker.SelectedIndexChanged += new EventHandler(pckAirport_SelectedIndexChanged);
-            PopulateAirportPicker(picker);
+        //    Picker picker = new Picker { Title = "Kies een luchthaven", Style = (Style)Application.Current.Resources["StandardPicker"], WidthRequest = 297, Margin = new Thickness(15, 0, 0, 0) };
+        //    picker.SelectedIndexChanged += new EventHandler(pckAirport_SelectedIndexChanged);
+        //    PopulateAirportPicker(picker);
 
-            Button button = new Button { Text = "X", WidthRequest = 50 };
-            button.Clicked += new EventHandler(BtnRemove_Clicked);
+        //    Button button = new Button { Text = "X", WidthRequest = 50 };
+        //    button.Clicked += new EventHandler(BtnRemove_Clicked);
 
-            stackLayout.Children.Add(picker);
-            stackLayout.Children.Add(button);
-            pickers.Add(picker);
+        //    stackLayout.Children.Add(picker);
+        //    stackLayout.Children.Add(button);
+        //    pickers.Add(picker);
 
-            return picker;
-        }
+        //    return picker;
+        //}
 
-        private bool Validate(Aircraft aircraft)
-        {
-            ValidationContext<Aircraft> validationContext = new ValidationContext<Aircraft>(aircraft);
-            ValidationResult validationResult = aircraftValidator.Validate(validationContext);
+        //private bool Validate(Aircraft aircraft)
+        //{
+        //    ValidationContext<Aircraft> validationContext = new ValidationContext<Aircraft>(aircraft);
+        //    ValidationResult validationResult = aircraftValidator.Validate(validationContext);
 
-            foreach (var error in validationResult.Errors)
-            {
-                if (error.PropertyName == nameof(aircraft.Registration))
-                {
-                    lblErrorRegistration.Text = error.ErrorMessage;
-                    lblErrorRegistration.IsVisible = true;
-                }
-                if (error.PropertyName == nameof(aircraft.AircraftType))
-                {
-                    lblErrorType.Text = error.ErrorMessage;
-                    lblErrorType.IsVisible = true;
-                }
-                if (error.PropertyName == nameof(aircraft.Airline))
-                {
-                    lblErrorAirline.Text = error.ErrorMessage;
-                    lblErrorAirline.IsVisible = true;
-                }
-                if (error.PropertyName == nameof(aircraft.Airports))
-                {
-                    lblErrorAirport.Text = error.ErrorMessage;
-                    lblErrorAirport.IsVisible = true;
-                }
-            }
-            return validationResult.IsValid;
-        }
+        //    foreach (var error in validationResult.Errors)
+        //    {
+        //        if (error.PropertyName == nameof(aircraft.Registration))
+        //        {
+        //            lblErrorRegistration.Text = error.ErrorMessage;
+        //            lblErrorRegistration.IsVisible = true;
+        //        }
+        //        if (error.PropertyName == nameof(aircraft.AircraftType))
+        //        {
+        //            lblErrorType.Text = error.ErrorMessage;
+        //            lblErrorType.IsVisible = true;
+        //        }
+        //        if (error.PropertyName == nameof(aircraft.Airline))
+        //        {
+        //            lblErrorAirline.Text = error.ErrorMessage;
+        //            lblErrorAirline.IsVisible = true;
+        //        }
+        //        if (error.PropertyName == nameof(aircraft.Airports))
+        //        {
+        //            lblErrorAirport.Text = error.ErrorMessage;
+        //            lblErrorAirport.IsVisible = true;
+        //        }
+        //    }
+        //    return validationResult.IsValid;
+        //}
 
-        private bool ValidateLastSeen(Aircraft aircraft)
-        {
-            bool result = true;
+        //private bool ValidateLastSeen(Aircraft aircraft)
+        //{
+        //    bool result = true;
 
-            lblErrorLastSeen.IsVisible = false;
+        //    lblErrorLastSeen.IsVisible = false;
 
-            if (aircraft.LastSeen < aircraft.FirstSeen)
-            {
-                lblErrorLastSeen.Text = "De datum van laatst gezien moet later of gelijk zijn aan eerst gezien";
-                lblErrorLastSeen.IsVisible = true;
-                result = false;
-            }
-            return result;
-        }
+        //    if (aircraft.LastSeen < aircraft.FirstSeen)
+        //    {
+        //        lblErrorLastSeen.Text = "De datum van laatst gezien moet later of gelijk zijn aan eerst gezien";
+        //        lblErrorLastSeen.IsVisible = true;
+        //        result = false;
+        //    }
+        //    return result;
+        //}
 
-        private void BtnRemove_Clicked(object sender, EventArgs e)
-        {
-            Button currentButton = sender as Button;
+        //private void BtnRemove_Clicked(object sender, EventArgs e)
+        //{
+        //    Button currentButton = sender as Button;
 
-            StackLayout parent = currentButton.Parent as StackLayout;
+        //    StackLayout parent = currentButton.Parent as StackLayout;
 
-            Picker picker = parent.Children.ElementAt(0) as Picker;
-            pickers.Remove(picker);
-            Picker lastPicker = pickers.LastOrDefault();
+        //    Picker picker = parent.Children.ElementAt(0) as Picker;
+        //    pickers.Remove(picker);
+        //    Picker lastPicker = pickers.LastOrDefault();
 
-            lastPicker.SelectedIndexChanged -= pckAirport_SelectedIndexChanged;
-            lastPicker.SelectedIndexChanged += new EventHandler(pckAirport_SelectedIndexChanged);
+        //    lastPicker.SelectedIndexChanged -= pckAirport_SelectedIndexChanged;
+        //    lastPicker.SelectedIndexChanged += new EventHandler(pckAirport_SelectedIndexChanged);
 
-            parent.Children.Clear();
-            AircraftForm.Children.Remove(parent);
-        }
+        //    parent.Children.Clear();
+        //    AircraftForm.Children.Remove(parent);
+        //}
 
-        private void pckAirport_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            AddPicker();
-            Picker currentPicker = sender as Picker;
-            currentPicker.SelectedIndexChanged -= pckAirport_SelectedIndexChanged;
+        //private void pckAirport_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    AddPicker();
+        //    Picker currentPicker = sender as Picker;
+        //    currentPicker.SelectedIndexChanged -= pckAirport_SelectedIndexChanged;
 
-            StackLayout parent = currentPicker.Parent as StackLayout;
-        }
+        //    StackLayout parent = currentPicker.Parent as StackLayout;
+        //}
 
-        private async void BtnSave_Clicked(object sender, EventArgs e)
-        {
-            busyIndicator.IsVisible = true;
+        //private async void BtnSave_Clicked(object sender, EventArgs e)
+        //{
+        //    busyIndicator.IsVisible = true;
 
-            SaveAircraftState();
+        //    SaveAircraftState();
 
-            if (Validate(currentAircraft) && ValidateLastSeen(currentAircraft))
-            {
-                if (isNew)
-                {
-                    currentAircraft.Id = Guid.NewGuid();
-                    await aircraftService.AddAsync(currentAircraft);
-                }
-                else
-                {
-                    await aircraftService.UpdateAsync(currentAircraft);
-                }
+        //    if (Validate(currentAircraft) && ValidateLastSeen(currentAircraft))
+        //    {
+        //        if (isNew)
+        //        {
+        //            currentAircraft.Id = Guid.NewGuid();
+        //            await aircraftService.AddAsync(currentAircraft);
+        //        }
+        //        else
+        //        {
+        //            await aircraftService.UpdateAsync(currentAircraft);
+        //        }
 
-                await DisplayAlert("Opgeslagen", $"het vliegtuig {currentAircraft.Registration} is opgeslagen", "Ok");
-                await Navigation.PopAsync();
-            }
-            busyIndicator.IsVisible = false;
-        }
+        //        await DisplayAlert("Opgeslagen", $"het vliegtuig {currentAircraft.Registration} is opgeslagen", "Ok");
+        //        await Navigation.PopAsync();
+        //    }
+        //    busyIndicator.IsVisible = false;
+        //}
     }
 }
