bb05e4c8b1c29afed343e1fb52258670082dab41
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-21 11:47:28 +0100

    Add converter to convert bool value to yes or no #16

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Converters/BoolToYesNoConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.Converters
+{
+    public class BoolToYesNoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isTrue)
+            {
+                string stringValue;
+
+                if (isTrue)
+                {
+                    stringValue = "Ja";
+                }
+                else
+                {
+                    stringValue = "Nee";
+                }
+                return stringValue;
+            }
+            throw new ArgumentException($"{nameof(value)} must be of value bool");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
