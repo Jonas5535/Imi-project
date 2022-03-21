50ee39799e339c90e7fd8d3dda3e44198e20f743
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-21 13:03:43 +0100

    Delete unused code in code behind of AircraftDetailPage #16

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/AircraftViews/AircraftDetailPage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/AircraftViews/AircraftDetailPage.xaml.cs
@@ -11,16 +11,9 @@ namespace Imi.Project.Mobile.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AircraftDetailPage : ContentPage
     {
-        //private readonly ICRUDService<Aircraft> aircraftService;
-        //private Aircraft shownAircraft;
-
-        public AircraftDetailPage(/*Aircraft aircraft*/)
+        public AircraftDetailPage()
         {
             InitializeComponent();
-
-            //aircraftService = new MockAircraftService();
-
-            //shownAircraft = aircraft;
         }
 
         protected override void OnAppearing()
@@ -36,33 +29,5 @@ namespace Imi.Project.Mobile.Pages
             AircraftDetailViewModel viewModel = sender as AircraftDetailViewModel;
             lvAirports.HeightRequest = viewModel.ShownAircraft.Airports.Count * 41;
         }
-
-        //private void LoadAircraft()
-        //{
-        //    lblRegistration.Text = shownAircraft.Registration;
-        //    lblType.Text = shownAircraft.AircraftType;
-        //    lblAirline.Text = shownAircraft.Airline;
-
-        //    if (shownAircraft.HasSpecialLivery) lblLivery.Text = "Ja";
-        //    else lblLivery.Text = "Nee";
-
-        //    lblFirstSeen.Text = shownAircraft.FirstSeen.ToShortDateString();
-        //    lblLastSeen.Text = shownAircraft.LastSeen.ToShortDateString();
-
-        //    lvAirports.HeightRequest = shownAircraft.Airports.Count * 41;
-        //    lvAirports.ItemsSource = shownAircraft.Airports;
-        //}
-
-        //private async void BtnDelete_Clicked(object sender, EventArgs e)
-        //{
-        //    await aircraftService.DeleteAsync(shownAircraft.Id);
-
-        //    await Navigation.PopAsync();
-        //}
-
-        //private async void BtnEdit_Clicked(object sender, EventArgs e)
-        //{
-        //    await Navigation.PushAsync(new AircraftFormPage(shownAircraft));
-        //}
     }
 }
