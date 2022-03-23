7b8606859f41f228003920bedefe5a3bd895a5fb
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-23 15:07:02 +0100

    Add AirportsController and implement get endpoints #10

---
+++ src/Imi.Project.Api/Controllers/AirportsController.cs
@@ -0,0 +1,39 @@
+﻿using Imi.Project.Api.Core.Dtos.Airport;
+using Imi.Project.Api.Core.Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AirportsController : ControllerBase
+    {
+        private readonly IAirportService _airportService;
+
+        public AirportsController(IAirportService airportService)
+        {
+            _airportService = airportService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            IEnumerable<AirportListResponseDto> airports = await _airportService.ListAllAsync();
+
+            return Ok(airports);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            AirportDetailResponseDto airport = await _airportService.GetByIdAsync(id);
+            //TODO Add errorhandling
+
+            return Ok(airport);
+        }
+    }
+}
