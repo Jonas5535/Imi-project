11aad696a40e6f78cc8f81bd0c28a412e5addb28
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-23 15:41:56 +0100

    Add Post, Put and Delte endpoints to AircraftTypesController #10

--- src/Imi.Project.Api/Controllers/AircraftTypesController.cs
+++ src/Imi.Project.Api/Controllers/AircraftTypesController.cs
@@ -34,5 +34,31 @@ namespace Imi.Project.Api.Controllers
 
             return Ok(aircraftType);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(AircraftTypeRequestDto requestDto)
+        {
+            //TODO Add errorhandling
+            AircraftTypeListResponseDto responseDto = await _aircraftTypeService.AddAsync(requestDto);
+            return CreatedAtAction(nameof(Get), new { id = responseDto.Id }, responseDto);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(AircraftTypeRequestDto requestDto)
+        {
+            //TODO Add errorhandling
+
+            AircraftTypeDetailResponseDto responseDto = await _aircraftTypeService.UpdateAsync(requestDto);
+            return Ok(responseDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            //TODO Add errorhandling
+
+            await _aircraftTypeService.DeleteAsync(id);
+            return Ok();
+        }
     }
 }
