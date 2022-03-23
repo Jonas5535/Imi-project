8a7e3000244740bb87383abd3a5cefe35c5411cd
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-23 13:02:37 +0100

    Add datatype attributes to request dtos #9

--- src/Imi.Project.Api.Core/Dtos/Airline/AirlineRequestDto.cs
+++ src/Imi.Project.Api.Core/Dtos/Airline/AirlineRequestDto.cs
@@ -5,18 +5,23 @@ namespace Imi.Project.Api.Core.Dtos.Airline
     public class AirlineRequestDto : BaseDto
     {
         [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Text)]
         public string IATACode { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Text)]
         public string ICAOCode { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
+        [DataType (DataType.Text)]
         public string MainAirport { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Text)]
         public string HeadQuarter { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
