9c71f337ea4dcaab8844f6e43a486378fecb97ec
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-03-27 17:18:03 +0200

    Add validation on GetByIdAsync method in AircraftTypeService #27

--- src/Imi.Project.Api.Core/Services/AircraftTypeService.cs
+++ src/Imi.Project.Api.Core/Services/AircraftTypeService.cs
@@ -5,6 +5,7 @@ using Imi.Project.Api.Core.Infrastructure.Services;
 using Imi.Project.Api.Core.Mapping;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Services
@@ -41,16 +42,24 @@ namespace Imi.Project.Api.Core.Services
 
         public async Task<AircraftTypeDetailResponseDto> GetByIdAsync(Guid id)
         {
-            //TODO Check if id exists
+            AircraftTypeDetailResponseDto dto = new AircraftTypeDetailResponseDto();
+
+            if (!_aircraftTypeRepository.GetAll().Any(a => a.Id.Equals(id)))
+            {
+                dto.AddNotFound($"No aircrafttypes with id {id} exist");
+                return dto;
+            }
+
             AircraftType result = await _aircraftTypeRepository.GetByIdAsync(id);
-            AircraftTypeDetailResponseDto dto = result.MapToDetailDto();
+            dto = result.MapToDetailDto();
             return dto;
         }
 
         public async Task<IEnumerable<AircraftTypeListResponseDto>> ListAllAsync()
         {
             IEnumerable<AircraftType> result = await _aircraftTypeRepository.ListAllAsync();
-            IEnumerable<AircraftTypeListResponseDto> dtos = result.MapToListDto();
+
+            IEnumerable<AircraftTypeListResponseDto> dtos = result.OrderByDescending(a => a.ModifiedOn).MapToListDto();
             return dtos;
         }
 
