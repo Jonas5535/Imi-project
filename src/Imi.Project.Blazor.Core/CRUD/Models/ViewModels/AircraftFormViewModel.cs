c184e9853309880a416cb97fa2ba1adbc5acad9b
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-04-10 21:11:07 +0200

    Add date selectors for firstseen and lastseen #32

--- src/Imi.Project.Blazor.Core/CRUD/Models/ViewModels/AircraftFormViewModel.cs
+++ src/Imi.Project.Blazor.Core/CRUD/Models/ViewModels/AircraftFormViewModel.cs
@@ -11,7 +11,7 @@ namespace Imi.Project.Blazor.Core.CRUD.Models.ViewModels
 
         [Display(Name = "Registratienummer")]
         [Required(ErrorMessage = "{0} is verplicht")]
-        [DataType(DataType.Text)]
+        [DataType(DataType.Text, ErrorMessage = "{0} moet gewone tekst zijn")]
         [MinLength(6, ErrorMessage = "{0} moet {1} karakters lang zijn")]
         [MaxLength(6, ErrorMessage = "{0} moet {1} karakters lang zijn")]
         public string Registration { get; set; }
@@ -22,13 +22,13 @@ namespace Imi.Project.Blazor.Core.CRUD.Models.ViewModels
 
         [Display(Name = "Eerst gezien")]
         [Required(ErrorMessage = "{0} is verplicht")]
-        [DataType(DataType.Date)]
+        [DataType(DataType.Date, ErrorMessage = "{0} moet een datum zijn")]
         [CurrentDate(ErrorMessage = "De datum van {0} mag niet later zijn dan vandaag")]
         public DateTime FirstSeen { get; set; }
 
         [Display(Name = "Laatst gezien")]
         [Required(ErrorMessage = "{0} is verplicht")]
-        [DataType(DataType.Date)]
+        [DataType(DataType.Date, ErrorMessage = "{0} moet een datum zijn")]
         [CurrentDate(ErrorMessage = "De datum van {0} mag niet later zijn dan vandaag")]
         //[LaterThan(nameof(FirstSeen), ErrorMessage = "De datum van {0} mag zijn vroeger zijn dan {1}")] //Werkt niet.
         //TODO Hulp vragen aan lector
