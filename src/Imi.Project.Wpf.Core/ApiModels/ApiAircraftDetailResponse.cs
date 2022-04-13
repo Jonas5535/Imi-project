15fdddc7fe6679a45dae8175829791f3a432b4ff
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-04-13 14:19:12 +0200

    Add response models for aircraft detail and list #42

---
+++ src/Imi.Project.Wpf.Core/ApiModels/ApiAircraftDetailResponse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Imi.Project.Wpf.Core.ApiModels
+{
+    public class ApiAircraftDetailResponse
+    {
+        [JsonPropertyName("registration")]
+        public string Registration { get; set; }
+
+        [JsonPropertyName("hasSpecialLivery")]
+        public bool HasSpecialLivery { get; set; }
+
+        [JsonPropertyName("firstSeen")]
+        public DateTime FirstSeen { get; set; }
+
+        [JsonPropertyName("lastSeen")]
+        public DateTime LastSeen { get; set; }
+
+        [JsonPropertyName("image")]
+        public object Image { get; set; }
+
+        [JsonPropertyName("longitude")]
+        public object Longitude { get; set; }
+
+        [JsonPropertyName("latitude")]
+        public object Latitude { get; set; }
+
+        [JsonPropertyName("aircraftType")]
+        public ApiAircraftTypeResponse AircraftType { get; set; }
+
+        [JsonPropertyName("airline")]
+        public ApiAirlineResponse Airline { get; set; }
+
+        [JsonPropertyName("airports")]
+        public List<ApiAirportResponse> Airports { get; set; }
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+    }
+}
+}
