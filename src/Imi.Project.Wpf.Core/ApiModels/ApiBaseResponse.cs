19719e392fe4350164891d3d387dfd4dcf259adb
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-04-13 13:57:36 +0200

    Add ApiBaseResponse #42

---
+++ src/Imi.Project.Wpf.Core/ApiModels/ApiBaseResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace Imi.Project.Wpf.Core.ApiModels
+{
+    public class ApiBaseResponse<T>
+    {
+        public HttpStatusCode Status { get; set; }
+        public string ErrorMessage { get; set; }
+        public T Data { get; set; }
+    }
+}
