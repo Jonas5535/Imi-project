11389bb30611a4860477d8fa80d936b3f1bf5033
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-04-13 12:36:56 +0200

    Add base address to HttpRequestService #42

--- src/Imi.Project.Wpf.Core/HttprequestService.cs
+++ src/Imi.Project.Wpf.Core/HttprequestService.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Imi.Project.Wpf.Core
 {
-    public class HttprequestService
+    public class HttpRequestService
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private HttpClient _httpClient;
+
+        public HttpRequestService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:5001/Api/");
+        }
     }
 }
