60a90b36fb259a7e4a706ce7e827f6e912300cf7
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-04-13 14:09:58 +0200

    Add response models for aircrafttype, airline and airport #42

---
+++ src/Imi.Project.Wpf.Core/ApiModels/ApiAirportResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Imi.Project.Wpf.Core.ApiModels
+{
+    public class ApiAirportResponse
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("iataCode")]
+        public string IataCode { get; set; }
+
+        [JsonPropertyName("icaoCode")]
+        public string IcaoCode { get; set; }
+
+        [JsonPropertyName("addedOn")]
+        public DateTime AddedOn { get; set; }
+
+        [JsonPropertyName("modifiedOn")]
+        public DateTime ModifiedOn { get; set; }
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+    }
+}
