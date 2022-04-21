aacdbfb427f5fa2b41ae9958bfa05bbdbd58d61a
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-04-21 13:49:35 +0200

    Add httpClientHandler to mobile core

---
+++ src/Imi.Project.Mobile.Core/Domain/Services/WebApiClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Imi.Project.Mobile.Core.Domain.Services
+{
+    public class WebApiClient
+    {
+        private static HttpClientHandler ClientHandler()
+        {
+            var httpClientHandler = new HttpClientHandler();
+#if DEBUG
+            //allow connecting to untrusted certificates when running a DEBUG assembly
+            httpClientHandler.ServerCertificateCustomValidationCallback =
+                (message, cert, chain, errors) => { return true; };
+#endif
+            return httpClientHandler;
+        }
+    }
+}
