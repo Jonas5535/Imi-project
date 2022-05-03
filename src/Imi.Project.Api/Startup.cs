e828fdca51efa89995668385fb98cb4844de7335
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-05-03 17:22:40 +0200

    Refactor AirportsController so all endpoints return a BaseResponseModel

--- src/Imi.Project.Api/Startup.cs
+++ src/Imi.Project.Api/Startup.cs
@@ -5,93 +5,87 @@ using Imi.Project.Api.Infrastructure.Data;
 using Imi.Project.Api.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Imi.Project.Api
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            //Repository classes and interfaces
-            services.AddScoped<IAircraftTypeRepository, AircraftTypeRepository>();
-            services.AddScoped<IAirlineRepository, AirlineRepository>();
-            services.AddScoped<IAirportRepository, AirportRepository>();
-            services.AddScoped<IAircraftRepository, AircraftRepository>();
+			//Repository classes and interfaces
+			services.AddScoped<IAircraftTypeRepository, AircraftTypeRepository>();
+			services.AddScoped<IAirlineRepository, AirlineRepository>();
+			services.AddScoped<IAirportRepository, AirportRepository>();
+			services.AddScoped<IAircraftRepository, AircraftRepository>();
 
-            //Service classes and interfaces
-            services.AddScoped<IAircraftService, AircraftService>();
-            services.AddScoped<IAircraftTypeService, AircraftTypeService>();
-            services.AddScoped<IAirlineService, AirlineService>();
-            services.AddScoped<IAirportService, AirportService>();
+			//Service classes and interfaces
+			services.AddScoped<IAircraftService, AircraftService>();
+			services.AddScoped<IAircraftTypeService, AircraftTypeService>();
+			services.AddScoped<IAirlineService, AirlineService>();
+			services.AddScoped<IAirportService, AirportService>();
 
-            //Register the Swagger generator, defining 1 or more Swagger documents.
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Aircraft API", Version = "v1" });
-                var filePath = Path.Combine(AppContext.BaseDirectory, "ApiDocs.xml");
-                c.IncludeXmlComments(filePath);
-            });
+			//Register the Swagger generator, defining 1 or more Swagger documents.
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Aircraft API", Version = "v1" });
+				var filePath = Path.Combine(AppContext.BaseDirectory, "ApiDocs.xml");
+				c.IncludeXmlComments(filePath);
+			});
 
-            services.AddControllers();
-            services.AddCors();
-        }
+			services.AddControllers();
+			services.AddCors();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
+			// Enable middleware to serve generated Swagger as a JSON endpoint.
+			app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "IMI-Lampaert-Jonas");
-                c.RoutePrefix = string.Empty;
-            });
+			// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+			// specifying the Swagger JSON endpoint.
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "IMI-Lampaert-Jonas");
+				c.RoutePrefix = string.Empty;
+			});
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
 
-            app.UseCors(builder => builder.AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod());
-        }
-    }
+			app.UseCors(builder => builder.AllowAnyOrigin()
+				.AllowAnyHeader()
+				.AllowAnyMethod());
+		}
+	}
 }
