f3152c2397d15d0edfdc444d05f01f02bc00ded6
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-05-23 11:19:05 +0200

    Add external api controller

---
+++ src/Imi.Project.Vue/Controllers/ExternalApiController.cs
@@ -0,0 +1,23 @@
+﻿using Imi.Project.Vue.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Imi.Project.Vue.Controllers
+{
+    public class ExternalApiController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
