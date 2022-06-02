bbefcadf65b684fa319b8cf59184ce56b61b578a
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-06-02 19:33:35 +0200

    Add BusyView to AircraftTypeDetailPage

--- src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftTypeViewModels/AircraftTypeDetailViewModel.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftTypeViewModels/AircraftTypeDetailViewModel.cs
@@ -17,6 +17,18 @@ namespace Imi.Project.Mobile.ViewModels
             _aircraftTypeService = aircraftTypeService;
         }
 
+        private bool isBusy;
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set
+            {
+                isBusy = value;
+                RaisePropertyChanged();
+            }
+        }
+
         private AircraftType shownType;
 
         public AircraftType ShownType
@@ -47,7 +59,11 @@ namespace Imi.Project.Mobile.ViewModels
 
         private async Task GetDetails()
         {
+            IsBusy = true;
+
             BaseResponse<AircraftType> response = await _aircraftTypeService.GetByIdAsync(ShownType.Id);
+
+            IsBusy = false;
 
             if (!response.IsSucces)
             {
@@ -76,7 +92,11 @@ namespace Imi.Project.Mobile.ViewModels
 
                 if (answer is true)
                 {
+                    IsBusy = true;
+
                     BaseResponse<AircraftType> response = await _aircraftTypeService.DeleteAsync(ShownType.Id);
+
+                    IsBusy = false;
 
                     if (!response.IsSucces)
                         await CoreMethods.DisplayAlert(response.Status, response.ErrorMessage, "OK");
