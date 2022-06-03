a3bcd3d6e222ba9c98bd3aa3e1f93ac9dca22ab0
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-06-03 17:53:42 +0200

    Add reload button to AircraftPage

--- src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftViewModels/AircraftViewModel.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/AircraftViewModels/AircraftViewModel.cs
@@ -55,6 +55,13 @@ namespace Imi.Project.Mobile.ViewModels
                 await ListInit();
         }
 
+        public ICommand RefreshListCommand => new Command(
+            async () =>
+            {
+                await ListInit();
+            }    
+        );
+
         public ICommand OpenAircraftDetailPageCommand => new Command<Aircraft>(
             async (Aircraft aircraft) =>
             {
