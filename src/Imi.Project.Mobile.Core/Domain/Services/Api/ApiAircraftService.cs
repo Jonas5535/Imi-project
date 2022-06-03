372e8c461b10032c8f1f1121068e6e9ed2e3bd41
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-06-03 16:36:21 +0200

    Add ApiAircraftService

---
+++ src/Imi.Project.Mobile.Core/Domain/Services/Api/ApiAircraftService.cs
@@ -0,0 +1,58 @@
+﻿using Imi.Project.Mobile.Core.Domain.Interfaces;
+using Imi.Project.Mobile.Core.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Mobile.Core.Domain.Services.Api
+{
+    public class ApiAircraftService : IAircraftService
+    {
+        private readonly string _baseEndpoint;
+
+        public ApiAircraftService()
+        {
+            _baseEndpoint = "aircrafts";
+        }
+
+        public Task<BaseResponse<Aircraft>> AddAsync(AircraftFormModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<BaseResponse<Aircraft>> DeleteAsync(Guid id)
+        {
+            return await WebApiClient.GetApiResult<Aircraft>($"{_baseEndpoint}/{id}");
+        }
+
+        public Task<AircraftType[]> GetAircraftTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Airline[]> GetAirlines()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Airport[]> GetAirports()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<BaseResponse<Aircraft>> GetByIdAsync(Guid id)
+        {
+            return await WebApiClient.GetApiResult<Aircraft>($"{_baseEndpoint}/{id}");
+        }
+
+        public async Task<BaseResponse<ICollection<Aircraft>>> ListAllAsync()
+        {
+            return await WebApiClient.GetApiResult<ICollection<Aircraft>>(_baseEndpoint);
+        }
+
+        public Task<BaseResponse<Aircraft>> UpdateAsync(AircraftFormModel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
