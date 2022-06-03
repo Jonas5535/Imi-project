a8164314940fba230d49ae2218e147c7a0959200
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-06-03 13:23:49 +0200

    Add AircraftFormModel to mobile core

---
+++ src/Imi.Project.Mobile.Core/Domain/Models/AircraftFormModel.cs
@@ -0,0 +1,18 @@
+﻿using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+
+namespace Imi.Project.Mobile.Core.Domain.Models
+{
+    public class AircraftFormModel
+    {
+        public string Registration { get; set; }
+        public bool HasSpecialLivery { get; set; }
+        public DateTime FirstSeen { get; set; }
+        public DateTime LastSeen { get; set; }
+        public MediaFile Image { get; set; }
+        public Guid AirlineId { get; set; }
+        public Guid AircraftTypeId { get; set; }
+        public List<Guid> AirportIds { get; set; }
+    }
+}
