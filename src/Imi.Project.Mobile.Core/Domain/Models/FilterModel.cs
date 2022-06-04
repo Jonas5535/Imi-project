45678b7d30c7d7956be538b0e82d34d792cd1d78
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-06-04 21:53:58 +0200

    Add filtermodel

---
+++ src/Imi.Project.Mobile.Core/Domain/Models/FilterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Imi.Project.Mobile.Core.Domain.Models
+{
+    public class FilterModel
+    {
+        public string Registration { get; set; }
+        public string Type { get; set; }
+        public string Airline { get; set; }
+        public bool SpecialLivery { get; set; }
+    }
+}
