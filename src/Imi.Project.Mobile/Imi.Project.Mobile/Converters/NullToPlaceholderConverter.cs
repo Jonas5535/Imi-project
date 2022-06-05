5cebfc2fef39f53b35b57111b50c5ac8eecaf613
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-06-06 01:12:43 +0200

    Add converter that shows placeholder when value is null

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Converters/NullToPlaceholderConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.Converters
+{
+    public class NullToPlaceholderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return "Geen info";
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
