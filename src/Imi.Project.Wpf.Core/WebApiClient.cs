c16c84705e9a7f6ad4ddffe461a55b608d175cba
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-09-14 15:07:23 +0200

    Add method to perform get requests to WebApiClient

--- src/Imi.Project.Wpf.Core/WebApiClient.cs
+++ src/Imi.Project.Wpf.Core/WebApiClient.cs
@@ -1,8 +1,12 @@
-﻿using Newtonsoft.Json;
+﻿using Imi.Project.Shared;
+using Imi.Project.Wpf.Core.ApiModels;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Formatting;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +20,42 @@ namespace Imi.Project.Wpf.Core
             //prevent self-referencing loops when saving Json
             formatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
             return formatter;
+        }
+
+        public async static Task<ApiBaseResponse<T>> GetApiResult<T>(string endpoint)
+        {
+            ApiBaseResponse<T> response = new ApiBaseResponse<T>();
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.Timeout = TimeSpan.FromSeconds(10);
+                try
+                {
+                    response = await httpClient.GetFromJsonAsync<ApiBaseResponse<T>>($"{ApiBaseUri.ApiBaseAddress}/{endpoint}");
+                    return response;
+                }
+                catch (HttpRequestException ex)
+                {
+                    response.IsSucces = false;
+                    response.Status = "Communicatiefout";
+                    response.ErrorMessage = ex.Message;
+                    return response;
+                }
+                catch (TaskCanceledException)
+                {
+                    response.IsSucces = false;
+                    response.Status = "Time out";
+                    response.ErrorMessage = "Het ophalen of opslaan van de data is gestopt.";
+                    return response;
+                }
+                catch (Exception)
+                {
+                    response.IsSucces = false;
+                    response.Status = "Fout!";
+                    response.ErrorMessage = "Er is iets misgelopen tijdens het ophalen van de data";
+                    return response;
+                }
+            }
         }
     }
 }
