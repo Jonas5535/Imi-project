92dfe6cb60cca1bdfdf5ebd32dd40f53a48bd9ae
Jonas Lampaert <jonas.lampaert@student.howest.be>
2022-09-14 14:59:57 +0200

    Add WebApiClient

---
+++ src/Imi.Project.Wpf.Core/WebApiClient.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Wpf.Core
+{
+    public static class WebApiClient
+    {
+        private static JsonMediaTypeFormatter GetJsonFormatter()
+        {
+            var formatter = new JsonMediaTypeFormatter();
+            //prevent self-referencing loops when saving Json
+            formatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            return formatter;
+        }
+    }
+}
